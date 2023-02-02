WIP model correct now. telescopeMSE8x8 loss fixed. Next need to test the testing step and emd computation before finally training

sensor-data-compression-pytorch/autoencoder.py
@@ -81,12 +81,13 @@ class AutoEncoder(pl.LightningModule):
         self.decoder = nn.Sequential(
             nn.Linear(self.encoded_dim, 128),
             nn.ReLU(),
-            nn.Unflatten(1, self.shape),
+            nn.Unflatten(1, (8, 4, 4)),
             nn.ConvTranspose2d(
-                self.shape[2], 8, kernel_size=3, stride=2, padding=1, output_padding=1
+                8, 8, kernel_size=3, stride=2, padding=1, output_padding=1
             ),
+            nn.ReLU(),
             nn.ConvTranspose2d(
-                8, self.shape[0], kernel_size=3, stride=2, padding=1, output_padding=1
+                8, self.shape[0], kernel_size=3, stride=1, padding=1,
             ),
             nn.Sigmoid(),
         )
@@ -113,14 +114,14 @@ class AutoEncoder(pl.LightningModule):
         return optimizer
 
     def training_step(self, batch, batch_idx):
-        x, y = batch
+        x = batch
         x_hat = self(x)
         loss = self.loss(x, x_hat)
         self.log("train_loss", loss, on_epoch=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
+        x = batch
         x_hat = self(x)
         loss = self.loss(x, x_hat)
         self.log("val_loss", loss, on_epoch=True, prog_bar=True)
@@ -130,7 +131,7 @@ class AutoEncoder(pl.LightningModule):
         """
         TODO: Instead compute the EMD between the input and output
         """
-        x, y = batch
+        x = batch
         input_Q, cnn_deQ, cnn_enQ = self.predict(x)
         input_calQ = self.map_to_calq(input_Q)
         output_calQ_fr = self.map_to_calq(cnn_deQ)

sensor-data-compression-pytorch/autoencoder_datamodule.py
@@ -29,9 +29,10 @@ ARRANGE_MASK = torch.tensor([
 ])
 
 class AutoEncoderDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir, num_workers=8) -> None:
+    def __init__(self, data_dir, batch_size=500, num_workers=8) -> None:
         super().__init__()
         self.data_dir = data_dir
+        self.batch_size = batch_size
         self.num_workers = num_workers
         self.calq_cols = [f"CALQ_{i}" for i in range(48)]
         self.norm_data = None
@@ -69,7 +70,7 @@ class AutoEncoderDataModule(pl.LightningDataModule):
             ]
         )
         data = self.mask_data(data)
-        data = data[self.calq_cols].astype("float64")
+        data = data[self.calq_cols].astype("float32")
         # data.to_pickle(os.path.join(self.data_dir, "data.pkl"))
         print(f"Input data shape: {data.shape}")
 
@@ -91,6 +92,7 @@ class AutoEncoderDataModule(pl.LightningDataModule):
         self.max_data = self.max_data / 35.0  # normalize to units of transverse MIPs
         self.sum_data = self.sum_data / 35.0  # normalize to units of transverse MIPs
         self.shaped_data = self.prep_input(self.norm_data)
+        # TODO: pickle the shaped data. Start here.
 
     def train_dataloader(self):
         """

sensor-data-compression-pytorch/telescope_pt.py
@@ -112,14 +112,23 @@ for i in range(12):
     Remap_12_3[i, int(i / 4)] = 1
 
 
+Remap_48_36 = Remap_48_36.cuda()
+Remap_48_12 = Remap_48_12.cuda()
+Remap_12_3 = Remap_12_3.cuda()
+Weights_48_36 = Weights_48_36.cuda()
+
 def telescopeMSE2(y_true, y_pred):
     y_true = y_true.to(dtype=y_pred.dtype)
 
     # TC-level MSE
+    print(f"Before reshape y_pred.shape: {y_pred.shape}")
+    print(f"Before reshape y_true.shape: {y_true.shape}")
     y_pred_rs = torch.reshape(y_pred, (-1, 48))
     y_true_rs = torch.reshape(y_true, (-1, 48))
+    print(f"y_pred_rs.shape: {y_pred_rs.shape}")
+    print(f"y_true_rs.shape: {y_true_rs.shape}")
     loss_tc1 = torch.mean(
-        torch.square(y_true_rs - y_pred_rs) * torch.max(y_pred_rs, y_true_rs), axis=-1
+        torch.square(y_true_rs - y_pred_rs) * torch.maximum(y_pred_rs, y_true_rs), dim=-1
     )
 
     # map TCs to 2x2 supercells and compute MSE
@@ -127,21 +136,21 @@ def telescopeMSE2(y_true, y_pred):
     y_true_36 = torch.matmul(y_true_rs, Remap_48_36)
     loss_tc2 = torch.mean(
         torch.square(y_true_36 - y_pred_36)
-        * torch.max(y_pred_36, y_true_36)
+        * torch.maximum(y_pred_36, y_true_36)
         * Weights_48_36,
-        axis=-1,
+        dim=-1,
     )
 
     # map 2x2 supercells to 4x4 supercells and compute MSE
-    y_pred_12 = torch.matmul(y_pred_36, Remap_48_12)
-    y_true_12 = torch.matmul(y_true_36, Remap_48_12)
+    y_pred_12 = torch.matmul(y_pred_rs, Remap_48_12)
+    y_true_12 = torch.matmul(y_true_rs, Remap_48_12)
     y_pred_3 = torch.matmul(y_pred_12, Remap_12_3)
     y_true_3 = torch.matmul(y_true_12, Remap_12_3)
     loss_tc3 = torch.mean(
-        torch.square(y_true_3 - y_pred_3) * torch.max(y_pred_3, y_true_3), axis=-1
+        torch.square(y_true_3 - y_pred_3) * torch.maximum(y_pred_3, y_true_3), dim=-1
     )
 
-    return 4 * loss_tc1 + 2 * loss_tc2 + loss_tc3
+    return 4 * loss_tc1.mean() + 2 * loss_tc2.mean() + loss_tc3.mean()
 
 
 remap_8x8 = [
@@ -199,6 +208,7 @@ remap_8x8_matrix = torch.zeros(48 * 64, dtype=torch.float32).reshape((64, 48))
 for i in range(48):
     remap_8x8_matrix[remap_8x8[i], i] = 1
 
+remap_8x8_matrix = remap_8x8_matrix.cuda()
 
 def telescopeMSE8x8(y_true, y_pred):
     return telescopeMSE2(