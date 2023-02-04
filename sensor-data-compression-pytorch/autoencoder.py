WIP everything fixed up to EMD computation. Need to do that in float64

sensor-data-compression-pytorch/autoencoder.py
@@ -1,13 +1,12 @@
+import ot
 import time
 import torch
-import numpy as np
 import torch.nn as nn
 import pytorch_lightning as pl
 
-from telescope_pt import telescopeMSE8x8
-from utils.metrics import emd
-from utils_pt import unnormalize
-from multiprocessing import Pool
+from telescope_pt import telescopeMSE8x8, move_constants_to_gpu
+from autoencoder_datamodule import ARRANGE, ARRANGE_MASK
+from utils_pt import unnormalize, emd
 
 """
 Model: "encoder"
@@ -59,17 +58,30 @@ Non-trainable params: 0
 """
 
 
+CALQ_MASK = torch.tensor([
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,
+    1,1,1,1,0,0,0,0,
+    1,1,1,1,0,0,0,0,
+    1,1,1,1,0,0,0,0,
+    1,1,1,1,0,0,0,0,
+])
+
 class AutoEncoder(pl.LightningModule):
     """
     XDR AutoEncoder class
     """
 
-    def __init__(self, val_sum) -> None:
+    def __init__(self, accelerator="gpu") -> None:
         super().__init__()
 
         self.encoded_dim = 16
         self.shape = (1, 8, 8) # PyTorch defaults to (C, H, W)
-        self.val_sum = val_sum
+        self.val_sum = None
+        self.accelerator = accelerator
+        self.hex_metric = self.get_hex_metric()
 
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 8, kernel_size=3, stride=2, padding=1),
@@ -92,18 +104,69 @@ class AutoEncoder(pl.LightningModule):
             nn.Sigmoid(),
         )
         self.loss = telescopeMSE8x8
+        if accelerator == "gpu":
+            print("Moved constants to gpu")
+            move_constants_to_gpu()
+
+    def invert_arrange(self):
+        """
+        Invert the arrange mask
+        """
+        remap = []
+        hashmap = {} # cell : index mapping
+        found_duplicate_charge = len(ARRANGE[ARRANGE_MASK == 1]) > len(torch.unique(ARRANGE[ARRANGE_MASK == 1]))
+        for i in range(len(ARRANGE)):
+            if ARRANGE_MASK[i] == 1:
+                if found_duplicate_charge:
+                    if CALQ_MASK[i] == 1:
+                        hashmap[int(ARRANGE[i])] = i 
+                else:
+                    hashmap[int(ARRANGE[i])] = i
+        for i in range(len(torch.unique(ARRANGE))):
+            remap.append(hashmap[i])
+        return torch.tensor(remap)
 
     def map_to_calq(self, x):
         """
         Map the input/output of the autoencoder into CALQs orders
         """
-        return x.reshape(len(x), 48)
+        remap = self.invert_arrange()
+        image_size = self.shape[0] * self.shape[1] * self.shape[2]
+        reshaped_x = x.reshape(len(x), image_size)
+        reshaped_x[:, ARRANGE_MASK == 0] = 0
+        return reshaped_x[:, remap]
+
+    def set_val_sum(self, val_sum):
+        self.val_sum = val_sum
 
     def predict(self, x):
         decoded_Q = self(x)
         encoded_Q = self.encoder(x)
-        encoded_Q = np.reshape(encoded_Q, (len(encoded_Q), self.encoded_dim, 1))
-        return x, decoded_Q, encoded_Q
+        encoded_Q = torch.reshape(encoded_Q, (len(encoded_Q), self.encoded_dim, 1))
+        return decoded_Q, encoded_Q
+
+    # EMD constants
+    def get_hex_metric(self):
+        hexCoords = torch.tensor([
+            [0.0, 0.0], [0.0, -2.4168015], [0.0, -4.833603], [0.0, -7.2504044],
+            [2.09301, -1.2083969], [2.09301, -3.6251984], [2.09301, -6.042], [2.09301, -8.458794],
+            [4.18602, -2.4168015], [4.18602, -4.833603], [4.18602, -7.2504044], [4.18602, -9.667198],
+            [6.27903, -3.6251984], [6.27903, -6.042], [6.27903, -8.458794], [6.27903, -10.875603],
+            [-8.37204, -10.271393], [-6.27903, -9.063004], [-4.18602, -7.854599], [-2.0930138, -6.6461945],
+            [-8.37204, -7.854599], [-6.27903, -6.6461945], [-4.18602, -5.4377975], [-2.0930138, -4.229393],
+            [-8.37204, -5.4377975], [-6.27903, -4.229393], [-4.18602, -3.020996], [-2.0930138, -1.8125992],
+            [-8.37204, -3.020996], [-6.27903, -1.8125992], [-4.18602, -0.6042023], [-2.0930138, 0.6042023],
+            [4.7092705, -12.386101], [2.6162605, -11.177696], [0.5232506, -9.969299], [-1.5697594, -8.760895],
+            [2.6162605, -13.594498], [0.5232506, -12.386101], [-1.5697594, -11.177696], [-3.6627693, -9.969299],
+            [0.5232506, -14.802895], [-1.5697594, -13.594498], [-3.6627693, -12.386101], [-5.7557793, -11.177696],
+            [-1.5697594, -16.0113], [-3.6627693, -14.802895], [-5.7557793, -13.594498], [-7.848793, -12.386101]
+        ])
+        #normalize so that distance between small cells (there are 4 per TC) is 1
+        oneHexCell = 0.5 * 2.4168015
+        hexCoords = hexCoords / oneHexCell
+        # pairwise distances
+        hex_metric = ot.dist(hexCoords, hexCoords, 'euclidean')
+        return hex_metric
 
     # Pytorch Lightning specific methods
     def forward(self, x):
@@ -132,45 +195,23 @@ class AutoEncoder(pl.LightningModule):
         TODO: Instead compute the EMD between the input and output
         """
         x = batch
-        input_Q, cnn_deQ, cnn_enQ = self.predict(x)
-        input_calQ = self.map_to_calq(input_Q)
+        cnn_deQ, cnn_enQ = self.predict(x)
+        input_calQ = self.map_to_calq(x)
         output_calQ_fr = self.map_to_calq(cnn_deQ)
-        print(f"input_Q.shape: {input_Q.shape}")
-        print(f"input_calQ.shape: {input_calQ.shape}")
-        print("Restore normalization")
         # Compute EMD
-        input_calQ = np.array(
+        input_calQ = torch.stack(
             [input_calQ[i] * self.val_sum[i] for i in range(len(input_calQ))]
         )  # shape = (batch_size, 48)
-        output_calQ = unnormalize(output_calQ_fr.copy(), self.val_sum)  # ae_out
-
+        output_calQ = unnormalize(torch.clone(output_calQ_fr), self.val_sum)  # ae_out
+        return (input_calQ, output_calQ)
+    
+    def test_epoch_end(self, outputs):
+        input_calQ = torch.cat([output[0] for output in outputs])
+        output_calQ = torch.cat([output[1] for output in outputs])
         start_time = time.time()
-        with Pool() as p:
-            emd_list = p.starmap(emd, zip(input_calQ, output_calQ))
+        emd_list = [emd(input_calQ[i], output_calQ[i], self.hex_metric) for i in range(len(input_calQ))]
         print(f"EMD computation time: {time.time() - start_time}")
-        average_emd = np.mean(np.array(emd_list))
+        average_emd = torch.mean(torch.tensor(emd_list))
         print(f"Average EMD: {average_emd}")
+        self.log("average_emd", average_emd)
 
-        loss = self.loss(x, cnn_deQ)
-        metrics = {"average_emd": average_emd, "loss": loss}
-        self.log_dict(metrics)
-        return metrics
-
-
-"""
-_________________________________________________________________
-Model: "autoencoder"
-_________________________________________________________________
- Layer (type)                Output Shape              Param #   
-=================================================================
- input_1 (InputLayer)        [(None, 8, 8, 1)]         0         
-                                                                 
- encoder (Functional)        (None, 16)                2144      
-                                                                 
- decoder (Functional)        (None, 8, 8, 1)           2833      
-                                                                 
-=================================================================
-Total params: 4,977
-Trainable params: 4,977
-Non-trainable params: 0
-"""

sensor-data-compression-pytorch/autoencoder_datamodule.py
@@ -29,26 +29,28 @@ ARRANGE_MASK = torch.tensor([
 ])
 
 class AutoEncoderDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir, batch_size=500, num_workers=8) -> None:
+    def __init__(self, data_file, data_dir=None, batch_size=500, num_workers=8) -> None:
         super().__init__()
         self.data_dir = data_dir
+        self.data_file = data_file
         self.batch_size = batch_size
         self.num_workers = num_workers
         self.calq_cols = [f"CALQ_{i}" for i in range(48)]
-        self.norm_data = None
-        self.max_data = None
-        self.sum_data = None
         self.valid_split = 0.2
         self.val_max = None
         self.val_sum = None
-
-        self.prepare_data()
+        self.max_data = None
+        self.sum_dat = None 
+        self.train_data = None 
+        self.val_data = None
 
     @staticmethod
     def add_argparse_args(parent_parser):
         parser = parent_parser.add_argument_group("Dataset")
-        parser.add_argument("--data_dir", type=str)
+        parser.add_argument("--data_dir", type=str, default=None)
+        parser.add_argument("--data_file", type=str, default="data.npy")
         parser.add_argument("--num_workers", type=int, default=8)
+        parser.add_argument("--batch_size", type=int, default=500)
         return parent_parser
 
     def mask_data(self, data):
@@ -57,7 +59,7 @@ class AutoEncoderDataModule(pl.LightningDataModule):
         """
         return data[data[self.calq_cols].sum(axis=1) != 0]
 
-    def load_data(self):
+    def load_data_dir(self):
         """
         Read and concat all csv files in the data directory into a single
         dataframe
@@ -71,10 +73,9 @@ class AutoEncoderDataModule(pl.LightningDataModule):
         )
         data = self.mask_data(data)
         data = data[self.calq_cols].astype("float32")
-        # data.to_pickle(os.path.join(self.data_dir, "data.pkl"))
         print(f"Input data shape: {data.shape}")
 
-        return data.values
+        return data
 
     def prep_input(self, norm_data, shape=(1, 8, 8)):
         """
@@ -86,24 +87,47 @@ class AutoEncoderDataModule(pl.LightningDataModule):
         print(f"Prepped shaped data shape: {shaped_data.shape}")
         return shaped_data
 
-    def prepare_data(self):
-        data = self.load_data()
-        self.norm_data, self.max_data, self.sum_data = normalize(data.copy())
-        self.max_data = self.max_data / 35.0  # normalize to units of transverse MIPs
-        self.sum_data = self.sum_data / 35.0  # normalize to units of transverse MIPs
-        self.shaped_data = self.prep_input(self.norm_data)
-        # TODO: pickle the shaped data. Start here.
+    def get_val_max_and_sum(self):
+        shaped_data, max_data, sum_data = self.process_data(save=False)
+        max_data = max_data / 35.0  # normalize to units of transverse MIPs
+        sum_data = sum_data / 35.0  # normalize to units of transverse MIPs
+        val_index = np.arange(int(len(shaped_data) * self.valid_split))
+        self.val_max = max_data[val_index]
+        self.val_sum = sum_data[val_index]
+        return self.val_max, self.val_sum
+    
+    def process_data(self, save=True):
+        """
+        Only need to run once to prepare the data and pickle it
+        """
+        data = self.load_data_dir()
+        norm_data, max_data, sum_data = normalize(data.values.copy())
+        shaped_data = self.prep_input(norm_data)
+        if save:
+            np.save(self.data_file, shaped_data)
+        return shaped_data, max_data, sum_data
+
+    # PyTorch Lightning specific methods
+    def setup(self, stage):
+        """
+        Load data from provided npy data_file
+        """
+        shaped_data = np.load(self.data_file)
+        print(f"Loaded shaped data shape: {shaped_data.shape}")
+
+        self.train_data = shaped_data[
+            int(len(shaped_data) * self.valid_split) :
+        ]
+        self.val_data = shaped_data[
+            : int(len(shaped_data) * self.valid_split)
+        ]
 
     def train_dataloader(self):
         """
         Return the training dataloader
         """
-        train_data = self.shaped_data[
-            int(len(self.shaped_data) * self.valid_split) :
-        ]
-
         return torch.utils.data.DataLoader(
-            train_data, batch_size=500, shuffle=True, num_workers=self.num_workers
+            self.train_data, batch_size=500, shuffle=True, num_workers=self.num_workers
         )
 
     def val_dataloader(self):
@@ -111,15 +135,8 @@ class AutoEncoderDataModule(pl.LightningDataModule):
         Return the validation dataloader
         """
         # Take the first valid_split% of the data as validation data
-        val_data = self.shaped_data[
-            : int(len(self.shaped_data) * self.valid_split)
-        ]
-        val_index = np.arange(int(len(self.shaped_data) * self.valid_split))
-        self.val_max = self.max_data[val_index]
-        self.val_sum = self.sum_data[val_index]
-
         return torch.utils.data.DataLoader(
-            val_data, batch_size=500, shuffle=False, num_workers=self.num_workers
+            self.val_data, batch_size=500, shuffle=False, num_workers=self.num_workers
         )
 
     def test_dataloader(self):
@@ -128,8 +145,4 @@ class AutoEncoderDataModule(pl.LightningDataModule):
         """
         return self.val_dataloader()
 
-    def get_val_max(self):
-        return self.val_max
     
-    def get_val_sum(self):
-        return self.val_sum

sensor-data-compression-pytorch/telescope_pt.py
@@ -111,22 +111,12 @@ Remap_12_3 = torch.zeros((12, 3))
 for i in range(12):
     Remap_12_3[i, int(i / 4)] = 1
 
-
-Remap_48_36 = Remap_48_36.cuda()
-Remap_48_12 = Remap_48_12.cuda()
-Remap_12_3 = Remap_12_3.cuda()
-Weights_48_36 = Weights_48_36.cuda()
-
 def telescopeMSE2(y_true, y_pred):
     y_true = y_true.to(dtype=y_pred.dtype)
 
     # TC-level MSE
-    print(f"Before reshape y_pred.shape: {y_pred.shape}")
-    print(f"Before reshape y_true.shape: {y_true.shape}")
     y_pred_rs = torch.reshape(y_pred, (-1, 48))
     y_true_rs = torch.reshape(y_true, (-1, 48))
-    print(f"y_pred_rs.shape: {y_pred_rs.shape}")
-    print(f"y_true_rs.shape: {y_true_rs.shape}")
     loss_tc1 = torch.mean(
         torch.square(y_true_rs - y_pred_rs) * torch.maximum(y_pred_rs, y_true_rs), dim=-1
     )
@@ -208,7 +198,13 @@ remap_8x8_matrix = torch.zeros(48 * 64, dtype=torch.float32).reshape((64, 48))
 for i in range(48):
     remap_8x8_matrix[remap_8x8[i], i] = 1
 
-remap_8x8_matrix = remap_8x8_matrix.cuda()
+def move_constants_to_gpu():
+    global remap_8x8_matrix, Remap_48_36, Remap_48_12, Remap_12_3, Weights_48_36
+    remap_8x8_matrix = remap_8x8_matrix.cuda()
+    Remap_48_36 = Remap_48_36.cuda()
+    Remap_48_12 = Remap_48_12.cuda()
+    Remap_12_3 = Remap_12_3.cuda()
+    Weights_48_36 = Weights_48_36.cuda()
 
 def telescopeMSE8x8(y_true, y_pred):
     return telescopeMSE2(

sensor-data-compression-pytorch/utils_pt.py
@@ -1,3 +1,5 @@
+import ot
+import torch
 import numpy as np
 
 def normalize(data, sumlog2=True):
@@ -25,7 +27,7 @@ def normalize(data, sumlog2=True):
 def unnormalize(norm_data, max_vals, sumlog2=True):
     for i in range(len(norm_data)):
         if sumlog2:
-            sumlog2 = 2 ** (np.floor(np.log2(norm_data[i].sum())))
+            sumlog2 = 2 ** (torch.floor(torch.log2(norm_data[i].sum())))
             norm_data[i] = (
                 norm_data[i] * max_vals[i] / (sumlog2 if sumlog2 else 1.0)
             )
@@ -36,3 +38,20 @@ def unnormalize(norm_data, max_vals, sumlog2=True):
                 / (norm_data[i].sum() if norm_data[i].sum() else 1.0)
             )
     return norm_data
+
+def emd(x, y, hex_metric, threshold=-1):
+    if (x.sum() == 0):
+        return -1.0
+    if (y.sum() == 0):
+        return -0.5
+    x = (1.0 / x.sum() if x.sum() else 1.0) * x.flatten()
+    y = (1.0 / y.sum() if y.sum() else 1.0) * y.flatten()
+
+    if threshold > 0:
+        # only keep entries above 2%, e.g.
+        x = torch.where(x > threshold, x, 0)
+        y = torch.where(y > threshold, y, 0)
+        x = 1.0 * x / x.sum()
+        y = 1.0 * y / y.sum()
+
+    return ot.emd2(x, y, hex_metric)