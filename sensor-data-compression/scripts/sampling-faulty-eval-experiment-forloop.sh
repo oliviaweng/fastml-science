6a6cadc536fe1312bb34d96c39f09aff39184b52
anmeza <anmeza@ucsd.edu>
2023-03-12 17:08:27 -0700

    Update enables single bit fault injection campaign to be executed using a bash script with a for loop (to temporarily overcome the kernel assign errors)

--- sensor-data-compression/scripts/sampling-faulty-eval-experiment-forloop.sh
+++ sensor-data-compression/scripts/sampling-faulty-eval-experiment-forloop.sh
@@ -9,23 +9,30 @@ OUTPUT_DIR=faulty-eval/sampling-ber1e-4-test
 LOGFILE=/home/anmeza/GitHub/fastml-science/sensor-data-compression/faulty-eval/end-of-life-emd-sampling-ber1e-4-test.csv
 
 # Sanity check
+bits=10600
+VMs=8
+system=0
+lbi=$((bits/VMs * system))
+hbi=$((bits/VMs * system + bits/VMs))
+for (( i=$lbi; i<$hbi ; i++ )); do 
 echo "Sanity check ber = 0"
 python3 sampling_faulty_eval_experiment.py \
-	-i $DATASET \
-	-o ${OUTPUT_DIR}/ber0 \
-	--AEonly 1 \
-	--nELinks 5 \
-	--models 8x8_c8_S2_tele_fqK \
-	--nrowsPerFile=4500000 \
-	--noHeader \
-	--pretrained-model $PRETRAINED_MODEL \
-	--eval-ber 0 \
-	--log-file $LOGFILE \
-	--efd_fp "./test_kernel_assign_v0-31_b9000_efd.log" \
-	--efr_fp "./test_kernel_assign_v0-31_b9000_efr.log" \
-	--efx_overwrite 0 \
-	--use_custom_bfr 1 \
-	--bfr_start 5000 \
-	--bfr_end   5001 \
-	--bfr_step  1 \
-	--num_val_inputs 32
+        -i $DATASET \
+        -o ${OUTPUT_DIR}/ber0 \
+        --AEonly 1 \
+        --nELinks 5 \
+        --models 8x8_c8_S2_tele_fqK \
+        --nrowsPerFile=4500000 \
+        --noHeader \
+        --pretrained-model $PRETRAINED_MODEL \
+        --eval-ber 0 \
+        --log-file $LOGFILE \
+        --efd_fp "./efd_emd_hesstrace_v0-31_b${lbi}-${hbi}-forloop.log" \
+        --efr_fp "./efr_emd_hesstrace_v0-31_b${lbi}-${hbi}-forloop.log" \
+        --efx_overwrite 0 \
+        --use_custom_bfr 1 \
+        --bfr_start $i \
+        --bfr_end   $((i+1)) \
+        --bfr_step  1 \
+        --num_val_inputs 32
+done
