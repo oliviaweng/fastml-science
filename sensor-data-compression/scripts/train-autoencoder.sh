e7daa277ada3f765a929bf64fefd0f9c02529e64
Olivia Weng <oliviajweng@gmail.com>
2023-04-21 15:11:06 -0700

    add 6bit fkeras bol trained model

--- sensor-data-compression/scripts/train-autoencoder.sh
+++ sensor-data-compression/scripts/train-autoencoder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DATASET=../../elegun-low-pt-high-eta/nElinks_5
-# DATASET=../../hgcal22data_signal_driven_ttbar_v11/nElinks_5/ 
+# DATASET=../../elegun-low-pt-high-eta/nElinks_5
+DATASET=../../hgcal22data_signal_driven_ttbar_v11/nElinks_5/ 
 # PRETRAINED_MODEL=/home/olivia/xdr/econ-t/fastml-science/sensor-data-compression/training/fkeras-quantized-begin-of-life-all-data/8x8_c8_S2_tele_fqK/8x8_c8_S2_tele_fqK.hdf5
 # PRETRAINED_MODEL=/home/olivia/xdr/econ-t/fastml-science/sensor-data-compression/training/fkeras-quantized-end-of-life/8x8_c8_S2_tele_fqK/8x8_c8_S2_tele_fqK.hdf5
 OUTPUT_DIR=training
@@ -9,10 +9,10 @@ LOGFILE=/home/olivia/xdr/econ-t/fastml-science/sensor-data-compression/training/
 
 python3 train.py \
 	-i $DATASET \
-	-o ${OUTPUT_DIR}/quantized-6bit-begin-of-life \
+	-o ${OUTPUT_DIR}/fkeras-6bit-end-of-life \
 	--AEonly 1 \
 	--nELinks 5 \
-	--models 8x8_c8_S2_tele_qK_6bit \
+	--models 8x8_c8_S2_tele_fqK_6bit \
 	--nrowsPerFile=4500000 \
 	--noHeader \
 	--log-file $LOGFILE
