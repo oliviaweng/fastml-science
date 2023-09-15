41503029366cc381d3d8e7a8b15f229e3a2b718f
Olivia Weng <olivweng@xir-blade-pxe-6.xilinx.com>
2023-09-15 10:31:09 +0100

    clean up no relu experiments

--- sensor-data-compression-pytorch/scripts/launch_seed_exp.sh
+++ sensor-data-compression-pytorch/scripts/launch_seed_exp.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-tmux new-session -d -s r00 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 0 random_seeds0.txt"
-tmux new-session -d -s r01 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 0 random_seeds1.txt"
-tmux new-session -d -s r02 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 0 random_seeds2.txt"
-tmux new-session -d -s r03 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 0 random_seeds3.txt"
-tmux new-session -d -s r14 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 1 random_seeds4.txt"
-tmux new-session -d -s r15 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 1 random_seeds5.txt"
-tmux new-session -d -s r16 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 1 random_seeds6.txt"
-tmux new-session -d -s r17 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 1 random_seeds7.txt"
-tmux new-session -d -s r28 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 2 random_seeds8.txt"
-tmux new-session -d -s r29 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 2 random_seeds9.txt"
-tmux new-session -d -s r210 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 2 random_seeds10.txt"
-tmux new-session -d -s r211 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 2 random_seeds11.txt"
+# tmux new-session -d -s r00 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 0 random_seeds0.txt"
+# tmux new-session -d -s r01 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 0 random_seeds1.txt"
+# tmux new-session -d -s r02 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 0 random_seeds2.txt"
+# tmux new-session -d -s r03 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 0 random_seeds3.txt"
+# tmux new-session -d -s r14 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 1 random_seeds4.txt"
+# tmux new-session -d -s r15 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 1 random_seeds5.txt"
+# tmux new-session -d -s r16 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 1 random_seeds6.txt"
+# tmux new-session -d -s r17 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 1 random_seeds7.txt"
+# tmux new-session -d -s r28 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 2 random_seeds8.txt"
+# tmux new-session -d -s r29 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 2 random_seeds9.txt"
+# tmux new-session -d -s r210 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 2 random_seeds10.txt"
+# tmux new-session -d -s r211 "source ~/.bash_profile; conda activate auto; ./scripts/run_seed_exp.sh 2 random_seeds11.txt"
 
 
 # tmux kill-session -t r00
@@ -26,3 +26,4 @@ tmux new-session -d -s r211 "source ~/.bash_profile; conda activate auto; ./scri
 # tmux kill-session -t r29
 # tmux kill-session -t r210
 # tmux kill-session -t r211
+
